4603bc28a1985c017637f84811dff8cf9d2d07d8
JoaoPaulo-dev20 <joaopauloalbuquerque5777@gmail.com>
2026-06-16 20:02:27 -0300

    feat: define rotas da aplicação

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ConsumidorController;
+use App\Http\Controllers\LeituraController;
+use App\Http\Controllers\FaturaController;
+use App\Http\Controllers\ConfiguracaoTaxaController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn() => redirect()->route('consumidores.index'));
+
+Route::resource('consumidores', ConsumidorController::class)->except(['show', 'destroy']);
+Route::get('/leituras/nova', [LeituraController::class, 'create'])->name('leituras.create');
+Route::post('/leituras', [LeituraController::class, 'store'])->name('leituras.store');
+Route::get('/faturas', [FaturaController::class, 'index'])->name('faturas.index');
+Route::patch('/faturas/{fatura}/pagar', [FaturaController::class, 'marcarPago'])->name('faturas.pagar');
+Route::get('/configuracao-taxa', [ConfiguracaoTaxaController::class, 'edit'])->name('configuracao.edit');
+Route::put('/configuracao-taxa', [ConfiguracaoTaxaController::class, 'update'])->name('configuracao.update');
