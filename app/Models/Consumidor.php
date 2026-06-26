a469b81827eebbac4e744dcde686900b919c526d
Francisco <francisco@example.com>
2026-06-26 14:52:48 -0300

    Ativa soft deletes em Consumidor

--- app/Models/Consumidor.php
+++ app/Models/Consumidor.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Consumidor extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'consumidores';
     protected $fillable = ['nome', 'endereco', 'numero_medidor', 'telefone'];
 
